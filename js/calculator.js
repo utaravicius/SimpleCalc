65105e097d2b8acfc12822618531807501af7d33
fierce-bastion-87108 <justaz23@gmail.com>
2018-11-30 15:41:27 +0200

    Second commit

--- js/calculator.js
+++ js/calculator.js
@@ -2,71 +2,101 @@ var currentField = "";
 var memory1 = "";
 var action = "";
 var result = "";
-var history = new Array();
-var historyItem = new Object();
+var istorija = new Array();
+var istorijaItem = new Object();
+var equalsWasPressed = false;
 
 function updateResultField(field) {
     document.getElementById('result').innerHTML = field;
 }
 
 function updateTable() {
-   document.getElementById('secondTable') .innerHTML = history[history.length-1];
-   console.log(history);
+    var table = document.getElementById('secondTable');
+
+    table.innerHTML = "<tr>" +
+        "<th>A</th>" +
+        "<th>Veiksmas</th>" +
+        "<th>B</th>" +
+        "<th>Rezultatas <input type='submit' id='up' value='up' onclick='sortResultUp()'>" +
+        "<input type='submit' id='down' value='down' onclick='sortResultDown()'></th>" +
+        "</tr>";
+
+    istorija.forEach(item => {
+        var row = "<tr>"
+            + "<td>" + item.number1 + "</td>"
+            + "<td>" + item.historyAction + "</td>"
+            + "<td>" + item.number2 + "</td>"
+            + "<td>" + item.rezultatas + "</td>"
+            + "</tr>";
+        table.innerHTML += row;
+    });
 }
 
 function handleClick(id) {
-    if (id != 'division' && id != 'multiplication' && id != 'subtract' && id != 'plus') {
-        if (id == 'dot') {
-            currentField += ".";
+
+    if(equalsWasPressed){
+        currentField = "";
+        equalsWasPressed = false;
+
+        if (id != 'division' && id != 'multiplication' && id != 'subtract' && id != 'plus') {
+            if (id == 'dot') {
+                currentField += ".";
+            } else {
+                currentField += id.valueOf();
+                updateResultField(currentField);
+            }
         } else {
-            currentField += id.valueOf();
-            updateResultField(currentField);
+            performAction(id);
         }
+
     } else {
-        performAction(id);
+
+        if (id != 'division' && id != 'multiplication' && id != 'subtract' && id != 'plus') {
+            if (id == 'dot') {
+                currentField += ".";
+            } else {
+                currentField += id.valueOf();
+                updateResultField(currentField);
+            }
+        } else {
+            performAction(id);
+        }
     }
 }
 
 function performAction(id) {
     switch (id) {
         case 'division':
-            action = "division"
+            action = "/"
             if (currentField != "") {
                 memory1 = currentField;
             }
             currentField = "";
             updateResultField(currentField);
-            console.log("Current status of action: " + action);
-
             break;
         case 'multiplication':
-            action = "multiplication"
+            action = "*"
             if (currentField != "") {
                 memory1 = currentField;
             }
             currentField = "";
             updateResultField(currentField);
-            console.log("Current status of action: " + action);
-
             break;
         case 'subtract':
-            action = "subtract"
+            action = "-"
             if (currentField != "") {
                 memory1 = currentField;
             }
             currentField = "";
             updateResultField(currentField);
-            console.log("Current status of action: " + action);
-
             break;
         case 'plus':
-            action = "plus"
+            action = "+"
             if (currentField != "") {
                 memory1 = currentField;
             }
             currentField = "";
             updateResultField(currentField);
-            console.log("Current status of action: " + action);
 
             break;
         default:
@@ -76,31 +106,62 @@ function performAction(id) {
 
 function calculate() {
     switch (action) {
-        case 'division':
+        case '/':
+            equalsWasPressed = true;
             result = Number(memory1) / Number(currentField);
             updateResultField(result);
-            addToHistory(historyItem = {number1: memory1, historyAction: action, number2: currentField, equals: result});
+            addToHistory(istorijaItem = {
+                number1: memory1,
+                historyAction: action,
+                number2: currentField,
+                rezultatas: result
+            });
+            console.log(istorijaItem);
             action = "";
             memory1 = "";
             currentField = result;
             break;
-        case 'multiplication':
+        case '*':
+            equalsWasPressed = true;
             result = Number(memory1) * Number(currentField);
             updateResultField(result);
+            addToHistory(istorijaItem = {
+                number1: memory1,
+                historyAction: action,
+                number2: currentField,
+                rezultatas: result
+            });
+
             action = "";
             memory1 = "";
             currentField = result;
             break;
-        case 'subtract':
+        case '-':
+            equalsWasPressed = true;
             result = Number(memory1) - Number(currentField);
             updateResultField(result);
+            addToHistory(istorijaItem = {
+                number1: memory1,
+                historyAction: action,
+                number2: currentField,
+                rezultatas: result
+            });
+
             action = "";
             memory1 = "";
             currentField = result;
             break;
-        case 'plus':
+        case '+':
+            equalsWasPressed = true;
             result = Number(memory1) + Number(currentField);
             updateResultField(result);
+            addToHistory(istorijaItem = {
+                number1: memory1,
+                historyAction: action,
+                number2: currentField,
+                rezultatas: result
+            });
+
             action = "";
             memory1 = "";
             currentField = result;
@@ -111,7 +172,20 @@ function calculate() {
 }
 
 function addToHistory(item) {
-    history.push(item);
-    updateTable()
+    istorija.push(item);
+    updateTable();
 }
 
+function sortResultUp() {
+    istorija = istorija.sort(function (obj1, obj2) {
+        return obj1.rezultatas - obj2.rezultatas;
+    });
+    updateTable();
+}
+
+function sortResultDown() {
+    istorija = istorija.sort(function (obj1, obj2) {
+        return obj2.rezultatas - obj1.rezultatas;
+    });
+    updateTable();
+}
